091c55e3e5b39c0995b1f17a80df8dac05bad380
Li Geng <ligeng@cbd-3d.com>
2021-11-17 13:11:12 +0800

    ...

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 const Header = () => {
 	return (
-		<header>
-			<div>
+		<header className="bg-gray-900 text-gray-100 shadow w-full">
+			<div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
 				<Link href="/">
-					<a>
+					<a className="flex md:w-1/5 title-font font-medium items-center md:justify-start mb-4 md:mb-0">
 						<Image
 							src="/images/logo.png"
 							width={40}
@@ -16,14 +16,14 @@ const Header = () => {
 						<span className="ml-3 tex-xl">DevSpace</span>
 					</a>
 				</Link>
-				<nav>
+				<nav className="flex flex-wrap md:w-4/5 items-center justify-end text-base md:ml-auto">
 					<Link href="/blog">
-						<a>
+						<a className="mx-5 cursor-pointer uppercase hover:text-indigo-300">
 							Blog
 						</a>
 					</Link>
 					<Link href="/about">
-						<a>
+						<a className="mx-5 cursor-pointer uppercase hover:text-indigo-300">
 							About
 						</a>
 					</Link>
