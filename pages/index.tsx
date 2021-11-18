9c46dcfc11e3002f0b2c35399f34b573893482ef
Kyung Lee <kyung.lee.official@outlook.com>
2021-11-18 23:00:53 +0800

    60. Display Posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,12 @@ import matter from "gray-matter";
 const HomePage: NextPage<any> = ({ posts }: { posts: any }) => {
 	return (
 		<Layout>
-			<h1>Hello World</h1>
+			<h1 className="text-5xl border-b-4 p-5 font-bold">Latest Posts</h1>
+			<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+				{posts.map((post: any, index: number) => (
+					<h3>{post.frontmatter.title}</h3>
+				))}
+			</div>
 		</Layout>
 	);
 };
