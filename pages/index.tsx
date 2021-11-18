e791beda671836ce509c9dd7351acc957c0769d8
Li Geng <ligeng@cbd-3d.com>
2021-11-18 19:23:41 +0800

    59. Get Posts & Parse Frontmatter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
+import fs from "fs";
+import path from "path";
 import type { NextPage } from "next";
 import Layout from "../components/Layout";
+import matter from "gray-matter";
 
 const HomePage: NextPage = () => {
 	return (
@@ -10,3 +13,18 @@ const HomePage: NextPage = () => {
 };
 
 export default HomePage;
+
+export async function getStaticProps() {
+	const files = fs.readdirSync(path.join("posts"));
+	const posts = files.map((filename) => {
+		const slug = filename.replace(".md", "");
+
+		return {
+			slug,
+		};
+	});
+
+	return {
+		props: {},
+	};
+}
