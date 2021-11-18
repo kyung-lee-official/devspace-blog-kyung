ab5c1cfc4ab15f40935e2a2eeea6887e27189a39
Li Geng <ligeng@cbd-3d.com>
2021-11-18 19:24:39 +0800

    ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,9 +18,15 @@ export async function getStaticProps() {
 	const files = fs.readdirSync(path.join("posts"));
 	const posts = files.map((filename) => {
 		const slug = filename.replace(".md", "");
+		const markdownWithMeta = fs.readFileSync(
+			path.join("posts", filename),
+			"utf-8"
+		);
+		const { data: frontmatter } = matter(markdownWithMeta);
 
 		return {
 			slug,
+			frontmatter
 		};
 	});
 
