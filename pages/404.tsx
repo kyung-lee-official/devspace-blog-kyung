54d64a610185ce8e521a41e1dacea71cccd3098e
Kyung Lee <kyung.lee.official@outlook.com>
2021-11-25 00:01:44 +0800

    ...

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,20 +1,22 @@
 import Layout from "@/components/layout/Layout";
+import { Row } from "antd";
 import Image from "next/image";
+import styles from "./404.module.css";
 
 const NotFoundPage = () => {
 	return (
 		<Layout title="Page Not Found">
-			<div>
-				<Image
-					src="/images/logo.png"
-					width={70}
-					height={70}
-				/>
+			<Row justify="center">
+				<div className={styles["image-row"]}>
+					<Image src="/images/logo.png" width={70} height={70} />
+				</div>
+			</Row>
+			<Row justify="center">
 				<h1>Whoops!</h1>
-				<h2>
-					This page does not exist.
-				</h2>
-			</div>
+			</Row>
+			<Row justify="center">
+				<h2>This page does not exist.</h2>
+			</Row>
 		</Layout>
 	);
 };
