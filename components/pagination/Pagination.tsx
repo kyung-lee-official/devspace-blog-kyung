0f474e459edf2ba290771dc80876ccd9c45046d0
Li Geng <ligeng@cbd-3d.com>
2021-11-24 13:40:47 +0800

    ...

--- components/pagination/Pagination.tsx
+++ components/pagination/Pagination.tsx
@@ -1,5 +1,40 @@
+import Link from "next/link";
+import styles from "./Pagination.module.css";
+
 const Pagination = ({ currentPage, numPages }: any) => {
-	return <div>Pagination</div>;
+	const isFirst = currentPage === 1;
+	const isLast = currentPage === numPages;
+	const prevPage = `/blog/page/${currentPage - 1}`;
+	const nextPage = `/blog/page/${currentPage + 1}`;
+
+	if (numPages === 1) {
+		// Show nothing if there is only one page.
+		return <></>;
+	}
+
+	return (
+		<div className={styles["pagination-container"]}>
+			<ul className={styles["pagination-ul"]}>
+				{!isFirst && (
+					<Link href={prevPage}>
+						<li className={styles["pagination-li"]}>Previous</li>
+					</Link>
+				)}
+
+				{Array.from({ length: numPages }, (_, i) => (
+					<Link href={`/blog/page/${i + 1}`}>
+						<li className={styles["pagination-li"]}>{i + 1}</li>
+					</Link>
+				))}
+
+				{!isLast && (
+					<Link href={nextPage}>
+						<li className={styles["pagination-li"]}>Next</li>
+					</Link>
+				)}
+			</ul>
+		</div>
+	);
 };
 
 export default Pagination;
