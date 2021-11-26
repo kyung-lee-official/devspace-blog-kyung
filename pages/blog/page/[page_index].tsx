4b6bf24b244b9e4c175958df43d42fdc233c56ff
Li Geng <ligeng@cbd-3d.com>
2021-11-26 13:57:41 +0800

    70. Category Sidebar

--- pages/blog/page/[page_index].tsx
+++ pages/blog/page/[page_index].tsx
@@ -72,6 +72,10 @@ export async function getStaticProps({ params }: any) {
 	const files = fs.readdirSync(path.join("posts"));
 	const posts = getPosts();
 
+	// Get categories for sidebar
+	const categories = posts.map((post) => post.frontmatter.category);
+	const uniqueCategories = [...new Set(categories)];
+
 	const numPages = Math.ceil(files.length / POST_PER_PAGE);
 	const pageIndex = page - 1;
 	const orderedPosts = posts.slice(
