d741fb02c9ea41aaf590930bf1d6afd27e261bc0
Li Geng <ligeng@cbd-3d.com>
2021-11-27 18:06:42 +0800

    ...

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,8 +1,29 @@
 import fs from "fs"
+import path from "path"
 import matter from "gray-matter"
-
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
+	let posts: any
+
+	if (process.env.NODE_ENV === "production") {
+		// @todo fetch from cache
+	} else {
+		const files = fs.readdirSync(path.join("posts"))
+		posts = files.map(filename => {
+			const markdownWithMeta = fs.readFileSync(path.join("posts", filename), "utf-8")
+			const { data: frontmatter } = matter(markdownWithMeta)
+			return {
+				frontmatter,
+			}
+		})
+	}
+
+	const result = posts.filter(({ frontmatter: { title, excerpt, category } }: any) =>
+		title.toLowerCase().indexOf(req.query.q) != -1 ||
+		excerpt.toLowerCase().indexOf(req.query.q) != -1 ||
+		category.toLowerCase().indexOf(req.query.q) != -1
+	)
+
 	res.status(200).json({ name: 'John Doe' })
 }
