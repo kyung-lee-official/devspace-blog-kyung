a0ecd8e61a4496d05127d58124f4372553c98d00
Li Geng <ligeng@cbd-3d.com>
2021-11-27 18:24:56 +0800

    ...

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -19,11 +19,11 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
 		})
 	}
 
-	const result = posts.filter(({ frontmatter: { title, excerpt, category } }: any) =>
+	const results = posts.filter(({ frontmatter: { title, excerpt, category } }: any) =>
 		title.toLowerCase().indexOf(req.query.q) != -1 ||
 		excerpt.toLowerCase().indexOf(req.query.q) != -1 ||
 		category.toLowerCase().indexOf(req.query.q) != -1
 	)
 
-	res.status(200).json({ name: 'John Doe' })
+	res.status(200).json(JSON.stringify({ results }))
 }
